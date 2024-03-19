c3216f5cf359730176a91d370e3500e399659215
Maximilian Homann <maximilian.homann@pionira-gmbh.de>
2024-03-19 11:35:16 +0100

    Project init

---
+++ minesweeper/src/main/java/de/pionira/weichwarengemeinschaft/App.java
@@ -0,0 +1,13 @@
+package de.pionira.weichwarengemeinschaft;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
